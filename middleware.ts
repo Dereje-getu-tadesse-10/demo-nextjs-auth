b31e75753f0cf6108d81c49c8be3f1b2a2a9a571
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-03-14 14:25:14 +0100

    Update

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,7 @@ import { Role } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(req) {
-	// Redirect if they don't have the appropriate role
-	console.log(req.nextauth.token?.role);
-	if (
-		req.nextUrl.pathname.startsWith("/admin") &&
-		req.nextauth.token?.role !== Role.ADMIN
-	) {
+	if (req.nextUrl.pathname.startsWith("/admin") && req.nextauth.token?.role !== Role.ADMIN) {
 		return NextResponse.redirect(new URL("/", req.url));
 	}
 });
