4ed8fbd4a8c1a700f8d6b1061097c3bf6e06cc94
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-03-14 13:38:04 +0100

    update: test login page render

--- pages/login.tsx
+++ pages/login.tsx
@@ -53,6 +53,9 @@ export default function Login({csrfToken, callbackUrl, providers}: LoginProps) {
 
     return (
           <>
+              <>
+                test
+              </>
               <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
                   <div className="sm:mx-auto sm:w-full sm:max-w-md">
                       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
